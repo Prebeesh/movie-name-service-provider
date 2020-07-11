a20bb7950564932de2f76614d9dc0ffa59aeb5e5
Prebeesh.S <prebeesh1427@gmail.com>
2020-07-11 18:43:44 +0530

    Created a configuration file and separated the bean creation methods

--- src/main/java/com/prebeesh1427/movienameserviceprovider/MovieNameServiceProviderApplication.java
+++ src/main/java/com/prebeesh1427/movienameserviceprovider/MovieNameServiceProviderApplication.java
@@ -11,10 +11,5 @@ public class MovieNameServiceProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieNameServiceProviderApplication.class, args);
 	}
-	
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 
 }
